1f7688d61602ae03deb93bf80cb244fcd4ccafd4
sh.ham <sh.ham@ziple.biz>
2017-12-10 03:24:56 +0900

    예외처리 추가

--- src/site-scraper.py
+++ src/site-scraper.py
@@ -3,24 +3,44 @@ from bs4 import BeautifulSoup
 from google_news_url_crawler import GoogleNewsURLCrawler
 
 class SiteScraper:
-    #인자: url리스트
+    #인자: url리스
     def scrap(self, urlsToScrap):
         terms = []
-        for urlToScrap in urlsToScrap:
-            get = requests.get(urlToScrap)
-            beautifulSoap = BeautifulSoup(get.text)
 
-            body = beautifulSoap.find('body')
+        for urlToScrap in urlsToScrap:
+            try:
+                get = requests.get(urlToScrap)
+                beautifulSoap = BeautifulSoup(get.text)
+                body = beautifulSoap.find('body')
+
+            except Exception as message:
+                print(message)
+                continue;
+
             for tag in body.find_all():
                 if((tag.name != 'script') and (tag.name != 'style')):
                     if(tag.string != None):
                         import re
-                        terms += list(filter(lambda element: (element != None) and (element != ''), re.split(r'\s+|\t+|\n+|,|:|;', tag.string)))
+                        try:
+                            terms += list(filter(lambda element: (element != None) and (element != ''), re.split(r'\s+|\t+|\n+|,|:|;', tag.string)))
+
+                        except Exception as message:
+                            print(message)
+                            continue;
 
         return terms
 
 # 사용예시
-# googleNewsURLCrawler = GoogleNewsURLCrawler('stock market', 20)
-# siteScraper = SiteScraper()
-# print(str(len(siteScraper.scrap(googleNewsURLCrawler.next()))))
+googleNewsURLCrawler = GoogleNewsURLCrawler('stock market', 10)
+siteScraper = SiteScraper()
+terms = siteScraper.scrap(googleNewsURLCrawler.next())
+print(len(terms))
+print(terms)
+terms = siteScraper.scrap(googleNewsURLCrawler.next())
+print(len(terms))
+print(terms)
+terms = siteScraper.scrap(googleNewsURLCrawler.next())
+print(len(terms))
+print(terms)
+
 
