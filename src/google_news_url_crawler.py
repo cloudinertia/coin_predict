5dcf82214f86fcf8a31837e6ff747a121c795b61
sh.ham <sh.ham@ziple.biz>
2017-12-13 09:59:07 +0900

    뉴스 카테고리에서만 크롤링 하도록 수정

--- src/google_news_url_crawler.py
+++ src/google_news_url_crawler.py
@@ -5,14 +5,14 @@ class GoogleNewsURLCrawler:
     chromeDriver = None
 
     #인자1: 검색 키워드, 인자2: 한번의 검색 쿼리로 리턴할 결과들의 수
-    def __init__(self, keyword, pageSizeToRetreive):
+    def __init__(self, keyword = '', pageSizeToRetreive = 10):
         GoogleNewsURLCrawler.__InitializeChromeDriver()
         self.__initialize(keyword, pageSizeToRetreive)
 
     def __initialize(self, keyword, pageSizeToRetreive):
         self.pageCount = 0
         self.pageSizeToRetreive = pageSizeToRetreive
-        self.queryExpression = GoogleNewsURLCrawler.__GetFormattedQueryExpressionForGoogleSearch(keyword)
+        self.queryExpression = GoogleNewsURLCrawler._GetFormattedQueryExpressionForGoogleSearch(keyword)
 
     #내부 유틸(private)
     @classmethod
@@ -24,7 +24,7 @@ class GoogleNewsURLCrawler:
 
     # 내부 유틸(private)
     @classmethod
-    def __GetFormattedQueryExpressionForGoogleSearch(cls, keyword):
+    def _GetFormattedQueryExpressionForGoogleSearch(cls, keyword):
         queryExpression = ''
 
         splittedKeywords = keyword.split()
@@ -39,29 +39,28 @@ class GoogleNewsURLCrawler:
 
     # 내부 유틸(private)
     @classmethod
-    def __getNextSearchURL(cls, queryExpression, pageCount, pageSizeToRetreive):
-        return 'https://www.google.com/search?q={}&start={}&tpe=nws&num={}&hl=en&tpe=nws'.format(queryExpression, pageCount, pageSizeToRetreive)
+    def _getNextSearchURL(cls, queryExpression, pageCount, pageSizeToRetreive):
+        return 'https://www.google.com/search?q={}&start={}&tbm=nws&num={}&hl=en'.format(queryExpression, pageCount, pageSizeToRetreive)
 
 
     #다음 크롤링 결과를 얻어옴
     def next(self):
-        searchURL = GoogleNewsURLCrawler.__getNextSearchURL(self.queryExpression, self.pageCount, self.pageSizeToRetreive)
+        urls = []
+
+        searchURL = GoogleNewsURLCrawler._getNextSearchURL(self.queryExpression, self.pageCount, self.pageSizeToRetreive)
         self.pageCount += self.pageSizeToRetreive
 
         self.chromeDriver.get(searchURL)
-        searchResults = self.chromeDriver.find_elements_by_class_name('rc')
+        searchResults = self.chromeDriver.find_elements_by_class_name('_hJs')
 
-        urls = []
         for searchResult in searchResults:
             try:
-                headLineOfSearchResult = searchResult.find_element_by_tag_name('h3')
-                linkElement = headLineOfSearchResult.find_element_by_tag_name('a')
-                urls.append(linkElement.get_attribute('href'))
+               link = searchResult.find_element_by_tag_name('h3').find_element_by_tag_name('a').get_attribute('href')
+               date = searchResult.find_element_by_class_name('slp').find_element_by_class_name('_QHs').get_attribute('innerText')
+               urls.append((link, date))
             except Exception as errorMessage:
                 print(errorMessage)
                 continue
-
-
 
         return urls
 
@@ -71,7 +70,7 @@ class GoogleNewsURLCrawler:
         self.__initialize(keyword, pageSizeToRetreive)
 
 # 사용법 예시
-# googleNewsURLCrawler = GoogleNewsURLCrawler('stock market', 20)
+# googleNewsURLCrawler = GoogleNewsURLCrawler('stock market', 10)
 # print(googleNewsURLCrawler.next())
 # print('\n')
 # print(googleNewsURLCrawler.next())
